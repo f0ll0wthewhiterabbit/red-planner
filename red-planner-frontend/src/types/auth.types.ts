d92faf283dde4115a7f41b5eefd25f5a4088f9c1
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2024-07-03 22:30:56 +0300

    Added types

---
+++ red-planner-frontend/src/types/auth.types.ts
@@ -0,0 +1,18 @@
+export interface IAuthForm {
+  email: string
+  password: string
+}
+
+export interface IUser {
+  id: number
+  name?: string
+  email: string
+  workInterval?: number
+  breakInterval?: number
+  intervalsCount?: number
+}
+
+export interface IAuthResponse {
+  accessToken: string
+  user: IUser
+}
