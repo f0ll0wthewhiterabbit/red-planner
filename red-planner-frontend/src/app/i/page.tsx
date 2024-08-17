ef443489318ea0a00f362d46a51b56da28576460
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2024-08-17 20:22:41 +0300

    Implemented Statistics

--- red-planner-frontend/src/app/i/page.tsx
+++ red-planner-frontend/src/app/i/page.tsx
@@ -1,12 +1,21 @@
 import { Metadata } from 'next'
 
+import { Heading } from '@/components/ui/heading'
+
 import { NO_INDEX_PAGE } from '@/constants/seo.contants'
 
+import { Statistics } from './Statistics'
+
 export const metadata: Metadata = {
-  title: 'Dashboard',
+  title: 'Statistics',
   ...NO_INDEX_PAGE,
 }
 
 export default function DashboardPage() {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <Heading title='Statistics' />
+      <Statistics />
+    </div>
+  )
 }
